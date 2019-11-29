7c56909d6494b99823602897db71204b52d4bd4e
jordanbelouchat <jijow.bel@gmail.com>
2019-11-29 18:31:31 +0100

    Rajout des captures d'écran du script search_fichiers

--- search_fichiers.sh
+++ search_fichiers.sh
@@ -4,8 +4,8 @@ echo "Bienvenue $USER, Aujourd'hui nous sommes le $dateduj"
 
 read -p 'Dans quel repertoire voulez vous chercher le(s) fichier(s), aujourdhui ? ' directory
 
-echo -e "Vous avez choisi le repertoire " $directory .\n
-echo -e "voici le(s) fichier(s) trouvé(s) contenant le mot $2 : " .\n
+echo -e "Vous avez choisi le repertoire " $directory 
+echo -e "voici le(s) fichier(s) trouvé(s) contenant le mot $2 : " 
 
 Grep -il $2 $directory/$1
 
